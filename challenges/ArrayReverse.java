a11df1f3575d9534623fffa8336f83ad0e09e160
Amelia Waggoner <wildginger@wavecable.com>
2021-03-22 15:19:58 -0700

    made corrections to the java challenge

--- challenges/ArrayReverse.java
+++ challenges/ArrayReverse.java
@@ -1,50 +1,29 @@
 import java.util.Arrays;
 
 public class ArrayReverse {
-  public statis void main(String[] args) {
-    int[] testArray = {1, 2, 3, 4, 5, 6};
-    int[] reverseArr = reverseArr(testArray);
-    System.out.println(Arrays.toString(testArray));
-    System.out.println(Arrays.toString(reversedArray));
-
-    reverseArrIn(testArray);
-    System.out.println(Arrays.toString(testArray));
-  }
-
-  public statis void printArray(int[] input){
-    System.out.print("[");
-    for (int i = 0; i < input.length - 1; i++) {
-      System.out.print(input[i] + ", ");
+    public static void main(String[] args) {
+        int[] arr1 = {1, 2, 3, 4, 5, 6};
+        System.out.println(Arrays.toString(arr1));
+        System.out.println(Arrays.toString(reverseArray(arr1)));
+        System.out.println(Arrays.toString(reverseArrayInPlace(arr1)));
     }
-    System.out.print(input[input.length - 1]);
-    System.out.print("]");
-    System.out.println();
-  }
 
-  public static int[] reverseArr(int[] input){
-    int[] reversed = new int[input.length];
-    int temp;
-    for (int i = 0; i < input.length; i++){
-      reversed[input.length - 1] = input[i];
-
+    public static int[] reverseArray (int[] arr) {
+        int[] result = new int[arr.length];
+        for (int i = 0; i < result.length; i++) {
+            result[result.length-i-1] = arr[i];
+        }
+        return result;
     }
-    return reversed;
-  }
-   
-  public static int[] reverseArrInPlace(int[] array) {
-    int temp;
 
-    for (int i = 0; i < array.length/2; i++) {
-      temp = array[i];
-      array[i] = array[array.length - 1 - i];
-      array[array.length - 1 - i] = temp;
+    public static int[] reverseArrayInPlace (int[] arr) {
+        int tmp;
+        
+        for (int i = 0; i < arr.length / 2; i++) {
+            tmp = arr[i];
+            arr[i] = arr[arr.length-i-1];
+            arr[arr.length-i-1] = tmp;
+        }
+        return arr;
     }
-    return array;
-  }
-
-  }
-   public static void test () {
-     int x = -5;
-     System.out.println(x/2);
-   }
 }
